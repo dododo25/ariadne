d5ecf80a9cbb5da83d52168a7c0d2db14247c385
dododo <terekhov160997@gmail.com>
2023-09-14 19:06:58 +0300

    Initial commit

--- xml/src/main/java/com/dododo/ariadne/xml/common/util/XmlStateManipulatorUtil.java
+++ xml/src/main/java/com/dododo/ariadne/xml/common/util/XmlStateManipulatorUtil.java
@@ -1,11 +1,16 @@
 package com.dododo.ariadne.xml.common.util;
 
-import com.dododo.ariadne.core.contract.FlowchartContract;
 import com.dododo.ariadne.core.model.ChainState;
+import com.dododo.ariadne.core.model.ConditionalOption;
+import com.dododo.ariadne.core.model.CycleEntryState;
+import com.dododo.ariadne.core.model.CycleMarker;
 import com.dododo.ariadne.core.model.EntryState;
+import com.dododo.ariadne.core.model.Option;
+import com.dododo.ariadne.core.model.Reply;
 import com.dododo.ariadne.core.model.State;
 import com.dododo.ariadne.core.model.Text;
 import com.dododo.ariadne.core.model.Switch;
+import com.dododo.ariadne.xml.common.contract.XmlFlowchartContract;
 import com.dododo.ariadne.xml.common.model.ComplexState;
 import com.dododo.ariadne.xml.common.model.ComplexSwitch;
 import com.dododo.ariadne.xml.common.model.Marker;
@@ -20,7 +25,16 @@ public final class XmlStateManipulatorUtil {
     private XmlStateManipulatorUtil() {}
 
     public static void replace(State oldState, State newState) {
-        FlowchartContract contract = new XmlFlowchartContractAdapter() {
+        replaceStateOnRoots(oldState, newState);
+        removeStateFromChildrenRoots(oldState);
+
+        if (newState != null) {
+            newState.removeRoot(oldState);
+        }
+    }
+
+    private static void replaceStateOnRoots(State oldState, State newState) {
+        XmlFlowchartContract contract = new XmlFlowchartContractAdapter() {
 
             @Override
             public void accept(EntryState state) {
@@ -28,8 +42,33 @@ public final class XmlStateManipulatorUtil {
             }
 
             @Override
+            public void accept(CycleMarker marker) {
+                acceptChainState(marker);
+            }
+
+            @Override
+            public void accept(CycleEntryState state) {
+                acceptChainState(state);
+            }
+
+            @Override
             public void accept(Text text) {
                 acceptChainState(text);
+            }
+
+            @Override
+            public void accept(Reply reply) {
+                acceptChainState(reply);
+            }
+
+            @Override
+            public void accept(Option option) {
+                acceptChainState(option);
+            }
+
+            @Override
+            public void accept(ConditionalOption option) {
+                acceptChainState(option);
             }
 
             @Override
@@ -83,9 +122,76 @@ public final class XmlStateManipulatorUtil {
         };
 
         Stream.of(oldState.getRoots()).forEach(root -> root.accept(contract));
+    }
 
-        if (newState != null) {
-            newState.removeRoot(oldState);
-        }
+    private static void removeStateFromChildrenRoots(State state) {
+        XmlFlowchartContract contract = new XmlFlowchartContractAdapter() {
+
+            @Override
+            public void accept(ComplexState state) {
+                state.childrenStream()
+                        .forEach(child -> child.removeRoot(state));
+            }
+
+            @Override
+            public void accept(PassState state) {
+                acceptChainState(state);
+            }
+
+            @Override
+            public void accept(SwitchBranch state) {
+                acceptChainState(state);
+            }
+
+            @Override
+            public void accept(Marker marker) {
+                acceptChainState(marker);
+            }
+
+            @Override
+            public void accept(EntryState state) {
+                acceptChainState(state);
+            }
+
+            @Override
+            public void accept(CycleMarker marker) {
+                acceptChainState(marker);
+            }
+
+            @Override
+            public void accept(CycleEntryState state) {
+                acceptChainState(state);
+            }
+
+            @Override
+            public void accept(Text text) {
+                acceptChainState(text);
+            }
+
+            @Override
+            public void accept(Reply reply) {
+                acceptChainState(reply);
+            }
+
+            @Override
+            public void accept(Option option) {
+                acceptChainState(option);
+            }
+
+            @Override
+            public void accept(ConditionalOption option) {
+                acceptChainState(option);
+            }
+
+            private void acceptChainState(ChainState state) {
+                State next = state.getNext();
+
+                if (next != null) {
+                    next.removeRoot(state);
+                }
+            }
+        };
+
+        state.accept(contract);
     }
 }
