8d588c8eab61f9f9899b1de77523eec26a0faf77
dododo <terekhov160997@gmail.com>
2024-05-26 19:07:50 +0300

    Initial commit

--- xml/src/main/java/com/dododo/ariadne/xml/job/CollectStatesJob.java
+++ xml/src/main/java/com/dododo/ariadne/xml/job/CollectStatesJob.java
@@ -2,7 +2,15 @@ package com.dododo.ariadne.xml.job;
 
 import com.dododo.ariadne.common.exception.AriadneException;
 import com.dododo.ariadne.common.job.AbstractJob;
+import com.dododo.ariadne.jaxb.contract.JaxbFlowchartContract;
+import com.dododo.ariadne.jaxb.contract.JaxbFlowchartContractAdapter;
+import com.dododo.ariadne.jaxb.model.JaxbComplexState;
+import com.dododo.ariadne.jaxb.model.JaxbComplexSwitch;
+import com.dododo.ariadne.jaxb.model.JaxbMenu;
+import com.dododo.ariadne.jaxb.model.JaxbOption;
 import com.dododo.ariadne.jaxb.model.JaxbRootState;
+import com.dododo.ariadne.jaxb.model.JaxbSwitchBranch;
+import com.dododo.ariadne.jaxb.mouse.ParentFirstJaxbFlowchartMouse;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
@@ -41,9 +49,43 @@ public final class CollectStatesJob extends AbstractJob {
             JAXBContext context = JAXBContext.newInstance(JaxbRootState.class);
             Unmarshaller unmarshaller = context.createUnmarshaller();
 
+            JaxbFlowchartContract contract = new JaxbFlowchartContractAdapter() {
+                @Override
+                public void accept(JaxbRootState state) {
+                    acceptComplexState(state);
+                }
+
+                @Override
+                public void accept(JaxbMenu menu) {
+                    acceptComplexState(menu);
+                }
+
+                @Override
+                public void accept(JaxbOption option) {
+                    acceptComplexState(option);
+                }
+
+                @Override
+                public void accept(JaxbComplexSwitch complexSwitch) {
+                    acceptComplexState(complexSwitch);
+                }
+
+                @Override
+                public void accept(JaxbSwitchBranch switchBranch) {
+                    acceptComplexState(switchBranch);
+                }
+
+                private void acceptComplexState(JaxbComplexState state) {
+                    state.childrenStream()
+                            .forEach(child -> child.setRoot(state));
+                }
+            };
+
             ((JaxbRootState) unmarshaller.unmarshal(file))
                     .childrenStream()
                     .forEach(rootState::addChild);
+
+            new ParentFirstJaxbFlowchartMouse().accept(rootState, contract);
         } catch (SAXException | JAXBException e) {
             throw new AriadneException(e);
         }
